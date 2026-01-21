d947211f4d7177decaa26a70e3f5f6880845ab04
kaminenileela <kaminenileela1992@gmail.com>
2026-01-20 19:46:13 -0500

    adding ec2-jenkins creation

--- jenkins.sh
+++ jenkins.sh
@@ -3,8 +3,8 @@
 curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade -y
-yum install fontconfig java-21-openjdk
-yum install jenkins
+yum install fontconfig java-21-openjdk -y
+yum install jenkins -y
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
