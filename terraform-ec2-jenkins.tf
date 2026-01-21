0f97557a34a45c1d19dc636fd2b3af9f6c849516
kaminenileela <kaminenileela1992@gmail.com>
2026-01-20 19:20:37 -0500

    adding ec2-jenkins creation

--- terraform-ec2-jenkins.tf
+++ terraform-ec2-jenkins.tf
@@ -1,9 +1,14 @@
+resource "aws_key_pair" "ec2key" {
+    key_name = "ec2key"
+    public_key = file ("/c/devops/ec2key.pub") 
+}
+
 resource "aws_instance" "jenkins" {
     ami = "ami-07ff62358b87c7116"
     instance_type = "t3.small"
     vpc_security_group_ids = [ aws_security_group.ports.id ]
     user_data = file("jenkins.sh")
-    key_name = file ("/c/devops/ec2key.pem") 
+    key_name = aws_key_pair.ec2key.key_name 
 }
 
 resource "aws_security_group" "ports" {
